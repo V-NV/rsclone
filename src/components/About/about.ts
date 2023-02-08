54f801b814177fcead19b8af5f240f0404c8a7dd
Fedor Malinin <sadsparrow@mail.ru>
2023-02-08 22:56:24 +0600

    refactor: page 'About' adaptive

--- src/components/About/about.ts
+++ src/components/About/about.ts
@@ -5,8 +5,8 @@ export function AboutPage():void {
     main.innerHTML +=` 
     <section class="about-main-wrap">
       <div class="about-main-cont">
-        <div class="about-us-cont">
-          <p class="p-about-us">OUR TEAM</p>
+
+          <p class="about-title">OUR TEAM</p>
           <div class="team-container">
             <div class="member-container">
               <div class="member-img member_01"></div>
@@ -23,10 +23,9 @@ export function AboutPage():void {
             <h2 class="member-name">Fedor</h2>
             <span class="member-sub">What is he doing here?</span>
         </div>
-          </div>
         </div>
         <div class="about-us-cont">
-          <p class="p-about-us">ABOUT US</p>
+          <p class="about-title">ABOUT US</p>
           <p class="about-us-text">Our project it's a free open-source platform
              created to learn beginner-level coding. Our project features 
              interactive tutorials and exercises designed for people without 
@@ -35,7 +34,7 @@ export function AboutPage():void {
           </p>
         </div>
         <div class="about-why-cont">
-          <p class="p-about-why">WHY WAS OUR PROJECT CREATED?</p>
+          <p class="about-title">WHY WAS OUR PROJECT CREATED?</p>
           <p class="about-why-text">As far as beginner-level coding is 
             concerned, the internet mostly offers code reference textbooks 
             and courses where you just follow a tutor mindlessly.
@@ -47,7 +46,7 @@ export function AboutPage():void {
           </p>
         </div>
         <div class="about-free-box">
-           <p class="about-free">IS IT FREE?</p>
+           <p class="about-title">IS IT FREE?</p>
            <p class="about-bottom">Our courses are completely free.</p>
         </div>
     </div>
