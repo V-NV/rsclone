d608535ab3433a9dfb9f865b9c5b75631e35b9d1
AlexGorSer <taskekaminari@gmail.com>
2023-02-24 18:22:25 +0200

    fix: fix block Html

--- src/components/Storage/webStorage.ts
+++ src/components/Storage/webStorage.ts
@@ -1,11 +1,16 @@
-// import { IWebStorage } from "../../types/api.interface";
+import { IWebStorageUserData } from "../../types/api.interface";
 import { getUserPost } from "../Api/user-post.api";
-// window.addEventListener("load", async () => {
-//   webStorage.userPost = await getUserPost();
-// });
-
-const { userPost: userPost } = await getUserPost();
-export const webStorage = {
+import { ReviewTemplate, DataTest } from "../Main/ReviewTemplate";
+window.addEventListener("load", async () => {
+  const { userPost } = await getUserPost();
+  webStorage.userPost = userPost;
+  webStorage.userPost === undefined
+    ? ReviewTemplate(DataTest)
+    : ReviewTemplate(webStorage.userPost);
+});
+const userPost = undefined;
+// const { userPost: userPost } = await getUserPost();
+export const webStorage: IWebStorageUserData = {
   // user_storage: {
   //   username: "",
   //   user_session: "",
